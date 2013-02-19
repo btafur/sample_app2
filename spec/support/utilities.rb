942024ed8ba47ada7b18f0168aa76076f198716b
Bruno Tafur <bruno.tafur@gmail.com>
2013-02-19 12:15:40 -0500

    Finish updating users

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,9 @@
 include ApplicationHelper
+
+def sign_in(user)
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  #Sign in without Capybara.
+  cookies[:remember_token] = user.remember_token
+end

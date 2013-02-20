a7e8b84c9552dcb506acdba78823e6f223322000
Bruno Tafur <bruno.tafur@gmail.com>
2013-02-19 20:44:05 -0500

    Make gravatar secure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
